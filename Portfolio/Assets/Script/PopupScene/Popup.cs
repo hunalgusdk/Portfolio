fb2b7cea41fb4b4bba1624bddbeda55f3846f5ce
hyuna_92 <hunalgusdk@gmail.com>
2020-06-20 18:35:27 +0900

    [Popup] Popup 기능 추가

---
+++ Portfolio/Assets/Script/PopupScene/Popup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popup : MonoBehaviour
+{
+    [SerializeField]
+    Image _imgBackground;
+
+    [SerializeField]
+    Text _txtNumber;
+
+    [SerializeField]
+    Animator _animator;
+
+    [SerializeField]
+    Button _btnCreatePopup;
+
+    Action _callback;
+
+    void Awake()
+    {
+        _btnCreatePopup.onClick.AddListener(OnClickCreatePopup);
+    }
+
+    void OnClickCreatePopup()
+    {
+        _callback.Invoke();
+    }
+
+    public void Init(int number, Action callback)
+    {
+        _imgBackground.color = new Color(UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f));
+
+        _txtNumber.text = string.Format("Popup Number : {0}", number.ToString());
+
+        _callback = callback;
+    }
+
+    public void Close()
+    {
+        _animator.Play("CLOSE");
+    }
+
+    public void OnTrigger()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Portfolio/Assets/Script/PopupScene/PopupScene.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupScene : MonoBehaviour
+{
+    [SerializeField]
+    Button _btnCreatePopup;
+
+    Stack<Popup> _stack = new Stack<Popup>();  
+    void Awake()
+    {
+        _btnCreatePopup.onClick.AddListener(OnClickCreatePopup);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_stack.Count > 0)
+            {
+                Popup popup = _stack.Pop();
+
+                if (popup != null)
+                {
+                    popup.Close();
+                }
+            }
+        }
+    }
+
+    void OnClickCreatePopup()
+    {
+        Object obj = Resources.Load("PopupScene/Popup/Popup");
+
+        if (obj != null)
+        {
+            GameObject clone = Instantiate(obj, transform) as GameObject;
+            Popup popup = clone.GetComponent<Popup>();
+
+            if (popup != null)
+            {
+                _stack.Push(popup);
+                popup.Init(_stack.Count, OnClickCreatePopup);
+            }
+        }
+    }
+}
