0681302ee86aac0a98abbd77702620c57db3a1cc
hyuna_92 <hunalgusdk@gmail.com>
2020-06-27 19:39:40 +0900

    [Effect Pool] Object Pooling 을 통한 Effect Pool 기능 구현

---
+++ Portfolio/Assets/Script/Animation/Effect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect : MonoBehaviour
+{
+    bool _isActive;
+    public bool isActive { get { return _isActive; } }
+
+    private void OnEnable()
+    {
+        _isActive = true;
+    }
+
+    private void OnDisable()
+    {
+        _isActive = false;
+    }
+
+    public void Show(Vector3 pickingPosition)
+    {
+        transform.position = pickingPosition;
+        gameObject.SetActive(true);
+    }
+}
---
+++ Portfolio/Assets/Script/EffectPool/EffectPoolScene.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectPoolScene : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _particle;
+
+    [SerializeField]
+    int _defaultPoolSize;
+
+    List<Effect> _pool = new List<Effect>();
+
+    void Awake()
+    {
+        for (int i = 0; i < _defaultPoolSize; i++)
+        {
+            Create();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pickingPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            for (int i = 0; i < _pool.Count; i++)
+            {
+                Effect target = _pool[i];
+
+                if (!target.isActive)
+                {
+                    target.Show(pickingPosition);
+                    return;
+                }
+            }
+
+            Effect effect = Create();
+
+            if (effect != null)
+            {
+                effect.Show(pickingPosition);
+            }
+        }
+    }
+
+    Effect Create()
+    {
+        GameObject obj = Instantiate(_particle, transform);
+
+        if (obj != null)
+        {
+            Effect effect = obj.GetComponent<Effect>();
+
+            if (effect != null)
+            {
+                _pool.Add(effect);
+
+                return effect;
+            }
+        }
+
+        return null;
+    }
+}
